8fbd268a80143510b6a3494f50054c91d6a4e5b4
MikeEzane <mezaneus@gmail.com>
2025-08-17 11:28:19 -0400

    added feature branch

--- main.tf
+++ main.tf
@@ -52,4 +52,16 @@ resource "azurerm_linux_virtual_machine" "example" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
+  resource "azurerm_storage_account" "storage" {
+  name                     = "storageaccountnamefortodayclass"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "demo"
+  }
+}
 }
